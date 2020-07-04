b27bd613c0fb34d4d3750a4a6e3e5b39286a7650
ashish pal singh <ashujss11@gmail.com>
2020-07-04 19:37:11 +0530

    new commit

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
   pathPrefix: '',
-  siteUrl: 'https://rolwinreevan.com',
-  siteTitle: 'Rolwin Reevan',
-  siteDescription: 'Logbook of a software developer',
-  author: 'Rolwin Reevan',
+  siteUrl: 'https://priyankamishra.com',
+  siteTitle: 'Priyanka Mishra',
+  siteDescription: 'Personal Site',
+  author: 'Priyanka Mishra',
   postsForArchivePage: 3,
   defaultLanguage: 'en',
-  disqusScript:
-    process.env.DISQUS_SCRIPT || 'https://rolwinreevan.disqus.com/embed.js',
+  /* disqusScript:
+  process.env.DISQUS_SCRIPT || 'https://rolwinreevan.disqus.com/embed.js', */
   pages: {
     home: '/',
     projects: 'projects',
@@ -23,60 +23,4 @@ module.exports = {
   },
   contactFormUrl: process.env.CONTACT_FORM_ENDPOINT || 'https://getform.io/f/48f2ac64-29ae-4a38-b445-06ad67b1bc42',
   googleAnalyticTrackingId: process.env.GA_TRACKING_ID || '',
-  tags: {
-    javascript: {
-      name: 'javascript',
-      description:
-        'JavaScript is an object-oriented programming language used alongside HTML and CSS to give functionality to web pages.',
-      color: '#f0da50',
-    },
-    nodejs: {
-      name: 'Node.js',
-      description:
-        'Node.js is a tool for executing JavaScript in a variety of environments.',
-      color: '#90c53f',
-    },
-    rxjs: {
-      name: 'RxJS',
-      description:
-        'RxJS is a library for reactive programming using Observables, for asynchronous operations.',
-      color: '#eb428e',
-    },
-    typescript: {
-      name: 'typescript',
-      description:
-        'TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.',
-      color: '#257acc',
-    },
-    reactjs: {
-      name: 'reactjs',
-      description:
-        'React is an open source JavaScript library used for designing user interfaces.',
-      color: '#61dbfa',
-    },
-    gatsby: {
-      name: 'Gatsby.js',
-      description:
-        'A framework built over ReactJS to generate static page web application.  ',
-      color: '#6f309f',
-    },
-    html: {
-      name: 'HTML',
-      description:
-        'A markup language that powers the web. All websites use HTML for structuring the content.',
-      color: '#dd3431',
-    },
-    css: {
-      name: 'css',
-      description:
-        'CSS is used to style the HTML element and to give a very fancy look for the web application.',
-      color: '#43ace0',
-    },
-    python: {
-      name: 'python',
-      description:
-        'A general purpose programming language that is widely used for developing various applications.',
-      color: '#f9c646',
-    },
-  },
 };
