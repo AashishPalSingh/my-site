e2184e50237eeb196cb5b4221c22ddd111f38687
ashish pal singh <ashujss11@gmail.com>
2020-07-17 18:22:19 +0530

    notification request

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,28 @@
  */
 
 // You can delete this file if you're not using it
+const React = require('react');
+
+exports.onRenderBody = function ({ setPreBodyComponents }) {
+    setPreBodyComponents([React.createElement('script', {
+        key: 'notifocation', dangerouslySetInnerHTML: {
+            __html: `
+void function() {
+  Notification.requestPermission(function(result) {
+    if (result === 'granted') {
+      navigator.serviceWorker.ready.then(function(registration) {
+        registration.showNotification('Vibration Sample', {
+          body: 'Buzz! Buzz!',
+          icon: '../images/touch/chrome-touch-icon-192x192.png',
+          vibrate: [200, 100, 200, 100, 200, 100, 200],
+          tag: 'vibration-sample'
+        });
+      });
+    }
+  });
+}()
+    `,
+        },
+    }),
+    ])
+}
