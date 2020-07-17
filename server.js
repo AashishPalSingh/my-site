e0580ab2fa3cb32dcc667dd354f939ff97bc81ee
ashish pal singh <ashujss11@gmail.com>
2020-07-18 01:53:26 +0530

    modifying notification with image

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const webpush = require('web-push');
 const subscription = {
-    endpoint: 'https://fcm.googleapis.com/fcm/send/dkv-FY6jS84:APA91bF-X77-qrsZfO2GCWmGlqkmJOTcX8fDqY6rUD_A51z014_gLDJqZDEYqPtHTCGYaUS2hZpyWiZIVJIzXL88Sc2a2goTFLu0d1kPk2jikG0BtFxOQjDJiBYFdpNvGhRnHHHflcoS',
+    endpoint: 'https://fcm.googleapis.com/fcm/send/egjJm1Hh5h8:APA91bHZSBkH_maJ1gWXCQm5so2v3L72w6K9cbGjStz7i5rrgOBMZ4ma26Ek13AdtwLCXq0xA0efjhoP_DWOioeIser2Y_3vCTDaPODeiMxq2vcG2ch_WRBn2fMSe41yzGBazjGZ8rOG',
     keys: {
-        p256dh: 'BNPOlY9tSF--gZVAnDXwrqx_rQM64fD_Upu_SGhNq__M4EyBjLvz7y-LpXaF2DTiMuto_hq0oBO3jzsO5GMYaIk',
-        auth: 'PZV0iJnfB6tEVSiIdb8_Ug',
+        p256dh: 'BNLU7H2BviGt0ks8X_KLmd1EHZuFnXrL3vOYOoKr30tFcoHTqgDiDMaxNyL3LSHdDfOKxw2OaEgsaPbD_zNG4_c"',
+        auth: '04vJ6dtAeCeXoqh-jnY10g',
     },
 };
 const notification = JSON.stringify({
     title: 'ashish',
     body: 'I’ve just published “Web Push Notifications”.',
+    tag: 'ashsih'
 });
 webpush.setVapidDetails(
     'mailto:ashujss11@gmail.com',
     'BEoY-3tvKYWqT2_8W7-0NvXX6W5Z2L7HTFP0SOh2aa3Ue-kzv9T9wV2ffNkdexvRG1OOm5DBxu4rBC9xc8dS7ck',
     '5bCb-yUxXBE234uuHxT9hZKHpdSHeJ7BLvq70sE5XWI'
 );
-webpush.sendNotification(subscription, notification);
+webpush.sendNotification(subscription, notification);	
--- src/custom-sw-code.js
+++ src/custom-sw-code.js
@@ -1,15 +1,12 @@
-setTimeout(() => {
-    self.registration.showNotification("Hello, Eve! How you doing?");
-}, 10000);
-
 self.addEventListener('push', event => {
     let { data } = event;
     let textData = data.text();
     let jsondata = JSON.parse(textData);
+    let icon = '/images/icon.png';
     if (textData === 'TERMINATE') {
         self.registration.unregister();
         return;
     } else {
-        self.registration.showNotification(jsondata.title, { body: jsondata.body });
+        self.registration.showNotification(jsondata.title, { body: jsondata.body, tag: jsondata.tag, icon });
     }
 })
